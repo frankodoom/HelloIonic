760bb780483a7717b1fcf1aba1d17b4895f41c32
FrankOdoom <f.odoom@outlook.com>
2017-11-25 17:39:05 +0000

    Ädded Intro Screen

---
+++ src/pages/intro/intro.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController } from 'ionic-angular';
+
+import { HomePage } from '../home/home';
+/**
+ * Generated class for the IntroPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-intro',
+  templateUrl: 'intro.html',
+})
+export class IntroPage {
+  constructor(public navCtrl: NavController) {}
+
+  navHome() {
+    this.navCtrl.setRoot(HomePage);
+  }
+}
